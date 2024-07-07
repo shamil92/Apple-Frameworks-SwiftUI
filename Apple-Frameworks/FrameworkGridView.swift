09b3ef924f9ec817748b442c41d046af5823eb3b
Shamil Kammaev <skammaev@gmail.com>
2024-07-07 14:35:41 +0300

    app images and create new parent FrameworkGridView

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Шамиль Каммаев on 07/07/2024.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
